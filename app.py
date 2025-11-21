8c5a7e3f766c370df6f1041f62a3dda5a6a35d15
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-11-21 23:30:38 +0100

    feat: map and db

--- app.py
+++ app.py
@@ -2,23 +2,43 @@ import flask
 import feedparser
 import random
 import dateutil
+import folium
+import folium.plugins
 
 sources = [
     'https://feeds.services.tv2.dk/api/feeds/nyheder/rss',
     'https://www.dr.dk/nyheder/service/feeds/senestenyt'
 ]
 
-tentries = []
+entries = []
 
 for source in sources:
     data = feedparser.parse(source)
-    tentries.extend(data.entries)
+    entries.extend(data.entries)
 
 entries = sorted(
-    tentries,
+    entries,
     key=lambda d: dateutil.parser.parse(d.published),
     reverse=True
 )
+
+locs = ["Ukraine", "København", "Aarhus"]
+
+locmap = {
+    "Ukraine": [48.379433, 31.16558]
+}
+hmap = folium.Map([56, 10], zoom_start=7, tiles="Cartodb dark_matter")
+heat = []
+
+for entry in entries:
+    for word in entry.title.split():
+        print(word)
+        if word in locmap:
+            heat.append(locmap[word])
+folium.plugins.HeatMap(heat).add_to(hmap)
+
+hmap.get_root().height = "100%"
+hmap_html = hmap.get_root()._repr_html_()
 
 app = flask.Flask(__name__)
 
@@ -29,3 +49,8 @@ def index():
 @app.route('/stories')
 def stories():
     return flask.render_template('stories.html', stories=entries[:20])
+
+@app.route('/map')
+def map():
+    return hmap_html
+
---
+++ calc.py
@@ -0,0 +1,99 @@
+import time
+import feedparser
+import sqlite3
+import urllib.request
+import os
+import zipfile
+import csv
+
+sync_freq     = 15 * 60 # every x seconds
+feed_file     = 'feeds'
+db_name       = 'story.db'
+cities500_url = 'https://download.geonames.org/export/dump/cities500.zip'
+cities500_zip = 'cities500.zip'
+cities500_dir = 'cities500'
+cities500     = 'cities500.txt'
+
+conn = sqlite3.connect(db_name)
+
+def cities500_init():
+    if not os.path.isfile(cities500):
+        urllib.request.urlretrieve(cities500_url, cities500_zip)
+        with zipfile.ZipFile(cities500_zip, 'r') as zip_ref:
+            zip_ref.extractall(cities500_dir)
+        os.remove(cities500_zip)
+        os.rename(os.path.join(cities500_dir, cities500), cities500)
+        os.rmdir(cities500_dir)
+
+def db_init():
+    cur  = conn.cursor()
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS story(
+            id INTEGER PRIMARY KEY, title varchar(200) NOT NULL, url varchar(200) NOT NULL
+        )"""
+    )
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS loc(
+            id INTEGER PRIMARY KEY, name varchar(200), lat float, lon float
+        )"""
+    )
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS link(
+            story INTEGER, loc INTEGER, PRIMARY KEY (story, loc)
+        )"""
+    )
+    conn.commit()
+
+def db_cities_fill():
+    cur = conn.cursor()
+    with open(cities500) as citiesfile:
+        reader = csv.reader(citiesfile, delimiter='\t')
+        for row in reader:
+            if "\"" in row[1]:
+                continue
+            s = """INSERT OR REPLACE INTO loc (id, name, lon, lat)
+                   VALUES({id}, \"{name}\", {lon}, {lat})"""
+            s = s.format(id=row[0], name=row[2], lon=row[4], lat=row[5])
+            cur.execute(s)
+    conn.commit()
+
+def setup():
+    cities500_init()
+    db_init()
+    db_cities_fill()
+
+def load_feeds() -> [str]:
+    with open(feed_file) as file:
+        return [line.rstrip() for line in file]
+
+def story_exists(url: str) -> bool:
+    s = "SELECT * FROM story WHERE url=\"{url}\""
+    s = s.format(url=url)
+    cur = conn.cursor()
+    cur.execute(s)
+    return cur.fetchone() is not None
+
+def sync():
+    feeds = load_feeds()
+    cur  = conn.cursor()
+    print('loading feeds...')
+    for feed in feeds:
+        data    = feedparser.parse(feed)
+        entries = data.entries
+        for entry in entries:
+            title = entry.title.replace("\"", "\"\"")
+            url   = entry.link.replace("\"", "\"\"")
+            if story_exists(url):
+                continue
+            print(title)
+            s = """INSERT INTO story (title, url)
+                   VALUES(\"{title}\", \"{url}\")"""
+            s = s.format(title=title, url=url)
+            cur.execute(s)
+    conn.commit()
+    conn.close()
+
+setup()
+while True:
+    sync()
+    time.sleep(sync_freq)
